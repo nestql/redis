74cddac89781604430522f8f71ce40238bfd8d9d
Serge Närhi <sergenarhi@gmail.com>
2022-11-09 22:06:31 +0300

    fix(): reset sample app config

--- sample/simple-example/src/app.module.ts
+++ sample/simple-example/src/app.module.ts
@@ -5,20 +5,10 @@ import { RedisModule } from '@nestql/redis';
 
 @Module({
   imports: [
-    RedisModule.register([
-      {
-        name: 'test1',
-        url: 'redis://:authpassword@127.0.0.1:6380/4',
-      },
-      {
-        name: 'test2',
-        host: process.env.REDIS_HOST,
-        port: parseInt(process.env.REDIS_PORT),
-        db: parseInt(process.env.REDIS_DB),
-        password: process.env.REDIS_PASSWORD,
-        keyPrefix: process.env.REDIS_PRIFIX,
-      },
-    ]),
+    RedisModule.register({
+      host: 'localhost',
+      port: 6379,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
